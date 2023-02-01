9170fad64367ba0418aad5879484798646e6dfc6
NChrzaszcz-Admin <nonochrzaszcz@gmail.com>
2023-02-01 16:52:35 +0100

    Change load file

--- fr/naulan/supermariobros/test/maps_engine_test.py
+++ fr/naulan/supermariobros/test/maps_engine_test.py
@@ -1,3 +1,5 @@
+import os
+
 from unittest import TestCase
 
 from fr.naulan.supermariobros.src.entities.player import Player
@@ -9,31 +11,29 @@ class MapsEngineTest(TestCase):
         """
             Test with full empty tiles
         """
-        maps_engine = MapsGenerator()
+        maps_generator = MapsGenerator()
 
-        raw_data = open("./res/matrices/empty.txt", "r")
-        maps_engine.new(raw_data.readlines(), raw_data.name, False)
-        self.assertTrue(len(maps_engine.data) == 1, "Have a map generated")
+        with open(os.getcwd() + "/res/matrices/empty.txt", "r") as f:
+            maps_generator.new(f.readlines(), f.name, False)
+        self.assertTrue(len(maps_generator.data) == 1, "Have a map generated")
 
-        empty_map = maps_engine.data[0]
+        empty_map = maps_generator.data[0]
         self.assertTrue(len(empty_map.data) == 0, "Is really empty")
-        raw_data.close()
 
     def test_player(self):
         """
             Test with only player on the map
         """
-        maps_engine = MapsGenerator()
+        maps_generator = MapsGenerator()
 
-        raw_data = open("./res/matrices/player.txt", "r")
-        maps_engine.new(raw_data.readlines(), raw_data.name, False)
-        self.assertTrue(len(maps_engine.data) == 1, "Have a map generated")
+        with open(os.getcwd() + "/res/matrices/player.txt", "r") as f:
+            maps_generator.new(f.readlines(), f.name, False)
+        self.assertTrue(len(maps_generator.data) == 1, "Have a map generated")
 
-        player_map = maps_engine.data[0]
+        player_map = maps_generator.data[0]
         self.assertTrue(len(player_map.data) == 1, "Have player into the list of entities")
         self.assertTrue(isinstance(player_map.player, Player), "Is really a player ?")
 
         player = player_map.player
         self.assertTrue(player.x == 4*16, "Generated at a good position on x axis")
         self.assertTrue(player.y == 16, "Generated at a good position on y axis")
-        raw_data.close()
